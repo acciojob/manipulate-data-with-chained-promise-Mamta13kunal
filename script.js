5ba6902c1f53812ca64f83fe1c7a4c9ba94ebc0d
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-10-01 19:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-function returnPromise(){
+/*function returnPromise(){
 	return Promise(resolve,reject)=>{
 		setTimeout(()=>{
 			resolve([1,2,3,4]);
@@ -18,4 +18,32 @@ setTimeout(()=>{
 		document.getElementById("output").innerHTML= multipliedData.join(",");
 	},2000);
 }).catch((e)=>{console.log("Code isn't correct")
-			  });
+			  });*/
+
+function returnPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+returnPromise()
+  .then((data) => {
+    setTimeout(() => {
+      // Filter odd numbers
+      const filteredData = data.filter((num) => num % 2 === 0);
+      document.getElementById("output").innerHTML = filteredData.join(", ");
+      return filteredData;
+    }, 1000);
+  })
+  .then((data) => {
+    setTimeout(() => {
+      // Multiply even numbers by 2
+      const multipliedData = data.map((num) => num * 2);
+      document.getElementById("output").innerHTML = multipliedData.join(", ");
+    }, 2000);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
