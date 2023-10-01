5be62730587fb64ac5f7f1fef844ae205fa8d314
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-10-01 19:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function returnPromise(){
+	return Promise(resolve,reject)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000)
+	};
+};
+returnPromise().then((data)=>{
+	setTimeout(()=>{
+		let filteredData=data.filter((num)=>num%2===0);
+		document.getElementById("output").innerHTML= filteredData.join(",");
+			return filteredData;
+	},1000);
+}).then((data)=>{
+setTimeout(()=>{
+	let multipliedData=data.map((num)=>num*2);
+		document.getElementById("output").innerHTML= multipliedData.join(",");
+	},2000);
+}).catch((e)=>{console.log("Code isn't correct")
+			  });
